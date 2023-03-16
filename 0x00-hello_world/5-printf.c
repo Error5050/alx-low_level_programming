a131d006872c77a1f81901a5964b98b92ef9e077
Error5050 <osama111315@gmail.com>
2023-03-16 02:33:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed by a newline
  * Return Always 0.
  */
 int main(void)
